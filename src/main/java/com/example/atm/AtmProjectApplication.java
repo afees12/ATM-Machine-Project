b219ecb1c9e8e9c9ba9cbf675c01b83a297b72f9
afees12 <114756054+afees12@users.noreply.github.com>
2024-07-11 16:13:02 +0530

    Add files via upload

---
+++ src/main/java/com/example/atm/AtmProjectApplication.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/atm/AtmProjectApplication.java
+package com.example.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AtmProjectApplication.class, args);
+    }
+}
